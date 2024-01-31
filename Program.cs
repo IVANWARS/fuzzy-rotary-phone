a0ee850d6beae77fd17f02e1ccc0f327c2eeba50
IVANWARS <IVANWARS230@yandex.ru>
2024-01-31 23:28:58 +0300

    решение задачи 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿ 
+﻿ int N = 322;
+  
+ {
+    if ( N % 2 == 0)
+    {
+        Console.WriteLine("чётноне число");
+    }
+    else
+    {
+        Console.WriteLine("нечётное число");
+    }
+ }
